4dd48f2014d56af3a20d84deb409992f63b5ffae
mohamedamara1 <hamma.amara20@gmail.com>
2024-12-09 02:20:25 +0100

    stuff

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ else:
 # Load the trained model
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model('model.h5')  # Adjust path if necessary
+    model = tf.keras.models.load_model('models/model_v5.keras')  # Adjust path if necessary
     return model
 
 model = load_model()
